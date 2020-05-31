a8a9939f96cc1c1f9888259def696096f2916fb3
Jaylenlee <54731199+Jaylenlee@users.noreply.github.com>
2020-05-31 19:17:40 +0800

    Commit#2: ReactNative Login frontend + Firebase backend

--- Login.js
+++ Login.js
@@ -1,51 +1,56 @@
-import React, { Component } from 'react';
-import { StyleSheet, 
-		 View,
-		 Image,
-		 Text, 
-		 KeyboardAvoidingView } from 'react-native';
+import React from 'react';
+import {StyleSheet, View, Image, Text, KeyboardAvoidingView} from 'react-native';
 import LoginForm from './LoginForm';
 
-export default class Login extends Component {
-	render() {
-		return (
-			<KeyboardAvoidingView behavior = "padding" style = {styles.container}>
-				<View style = {styles.container}>
-					<View style = {styles.logoContainer}>
-						<Image 
-							style = {styles.logo}
-							source = {require('../../Images/phoventlogo.png')} 
-						/>
-						<Text style = {styles.title}>By HitMeUp2020 using React Native</Text>
-					</View>
-					<View style = {styles.formContainer}>
-						<LoginForm />
-					</View>
-				</View>
-			</KeyboardAvoidingView>
-		);
-	}
+
+export default class Login extends React.Component {
+
+    render(){
+        return(
+            <KeyboardAvoidingView behavior="padding" style = {styles.container}>
+                <View style = {styles.logoContainer}>
+                    <Image
+                        style = {styles.logo} 
+                        source ={require('../../pic/pic.JPG')}
+                    />
+                    <Text stye = {styles.title}>
+                        Phovent is the best
+                    </Text>
+                </View>
+               <View style = {styles.formContainer}>
+                    <LoginForm/>
+               </View>
+            </KeyboardAvoidingView>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: 'black'
-	},
-	logoContainer: {
-		alignItems: 'center',
-		flexGrow: 1,
-		justifyContent: 'center'
-	},
-	logo: {
-		width: 200,
-		height: 250
-	},
-	title: {
-		color: '#FFF',
-		marginTop: 10,
-		width: 160,
-		textAlign: 'center',
-		opacity: 0.9
-	}
+    container: {
+        flex: 1,
+        backgroundColor: '#3498db'
+    },
+
+    logo: {
+        width: 100,
+        height: 100,
+    },
+
+    logoContainer: {
+        alignItems: 'center',
+        flexGrow:1,
+        justifyContent:'center'
+    },
+
+    formContainer: {
+
+    },
+
+    title: {
+        color:  "#FFF",
+        marginTop: 10,
+        width: 140,
+        textAlign: 'center',
+        opacity: 0.9
+    }
 })
---
+++ Userlist.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, FlatList, ActivityIndicator, Text, StyleSheet } from 'react-native';
+import firebaseDb from './firebaseDb';
+
+export default class Userlist extends React.Component {
+    state = {
+        isLoading: true,
+        users: null
+    }
+
+    componentDidMount() {
+        firebaseDb.firestore().collection('users').get().then(querySnapshot => {
+        const results = []
+        querySnapshot.docs.map(documentSnapshot => results.push(documentSnapshot.data()))
+        this.setState({isLoading: false, users: results})
+        }).catch(err => console.error(err))
+    }
+
+    render() {
+        const { isLoading, users } = this.state
+        if (isLoading) {
+            return <ActivityIndicator />
+        }
+        return(
+            <FlatList
+                data={users}
+                renderItem={({item}) => (
+                <View style={styles.itemContainer}>
+                    <Text>{item.username}</Text>
+                    <Text>{item.email}</Text>
+                </View>
+                )}
+                style={styles.container}
+                keyExtractor={item => item.email}
+            />
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+    itemContainer: {
+        marginVertical: 20,
+    }
+})
