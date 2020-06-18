316a19fa14f1a693fd0654a92e0b6fe7c335f082
Jaylenlee <54731199+Jaylenlee@users.noreply.github.com>
2020-06-18 15:29:17 +0800

    Commit 5 - Edit Profile Done. Friend Request WIP.

---
+++ Phovent 18 June/src/Components/Event/PrivateInviteContainer.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Container, Header, Title, Content, Button, Left, Right, Body, Icon,
+         Text, ListItem, Thumbnail, Input, Item } from 'native-base';
+import Header1 from '../GlobalStyles/Header';
+import SubHeader from '../GlobalStyles/SubHeader';
+
+let helperArray = require('../Friend/SampleUserList.json');
+
+class PrivateInviteContainer extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state={
+            allUsers: helperArray,
+            usersFiltered: helperArray
+        }
+    }
+
+    searchUser(searchText) {
+        this.setState({
+            usersFiltered: this.state.allUsers.filter(i =>
+                           i.name.toLowerCase().includes(searchText.toLowerCase()))
+        })
+    }
+
+    render() {
+        return (
+            <View style={{backgroundColor: '#9eddff'}}>
+                <Header1 title="Event Creation"/>
+                <SubHeader title="Choose Invitees - Private"/>
+
+                <Header searchBar rounded style={{backgroundColor: '#c425e8'}}>
+                    <Item>
+                        <Icon name='search' />
+                        <Input placeholder="Search Friend"
+                               onChangeText={text => {this.searchUser(text)}}
+                        />
+                    </Item>
+                </Header>
+
+                <Content>
+                    {this.state.usersFiltered.map((item, index) => (
+                        <ListItem>
+                            <Left>
+                                <Thumbnail source={{uri: item.photo}} />
+                            </Left>
+                            <Body>
+                                <Text>{item.name}</Text>
+                                <Text note>{item.email}</Text>
+                                <Text note>{item.userID}</Text>
+                            </Body>
+
+                            <TouchableOpacity style={styles.addButton} >
+                                <Text style={styles.addButtonText}></Text>
+                            </TouchableOpacity>
+                        </ListItem>
+                    ))}
+                </Content>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+         addButton: {
+             width: 20,
+             backgroundColor: 'rgba(0, 157, 255, 0.8)',
+             alignItems: 'center',
+             justifyContent: 'center',
+             height: 20
+         }
+});
+
+export default PrivateInviteContainer
---
+++ Phovent 18 June/src/Components/Login/RegisterForm.js
@@ -0,0 +1,258 @@
+import React from 'react';
+import {StyleSheet, View, Text, TextInput, TouchableOpacity, StatusBar, ScrollView, SafeAreaView} from 'react-native';
+import {withNavigation} from 'react-navigation';
+import firebaseDb from '../Database/firebaseDb';
+import {Ionicons, MaterialIcons} from "@expo/vector-icons";
+
+class RegisterForm extends React.Component {
+
+    state = {
+        username: '',
+        email: '',
+        password: '',
+        signUpSuccess: false,
+        errorMessage: null,
+        gender: '',
+        age: '',
+        location: '',
+        occupation: '',
+        interests: '',
+    };
+
+    handleUpdateUsername = (username) => this.setState({username});
+    handleUpdateEmail = (email) => this.setState({email});
+    handleUpdatePassword = (password) => this.setState({password});
+    handleUpdateGender = (gender) => this.setState({gender});
+    handleUpdateAge = (age) => this.setState({age});
+    handleUpdateLocation = (location) => this.setState({location});
+    handleUpdateOccupation = (occupation) => this.setState({occupation});
+    handleUpdateInterests = (interests) => this.setState({interests});
+
+
+    handleCreateUser = () => {
+        const {username, email, password, signUpSuccess, errorMessage, gender,age,location,occupation, interests} = this.state;
+        firebaseDb
+            .auth
+            .createUserWithEmailAndPassword(email, password)
+            .then(userCredentials => {
+                const user = userCredentials.user;
+                firebaseDb.db
+                .collection('profile')
+                .doc(user.uid)
+                .set({
+                    username: username,
+                    email: email,
+                    gender: gender,
+                    age: Number(age),
+                    location: location,
+                    occupation: occupation,
+                    interests: interests,
+                    photo: "https://cdn4.iconfinder.com/data/icons/political-elections/50/46-512.png",// static photo atm
+                    uid: user.uid,
+                }).catch(error => this.setState({errorMessage: error.message}));
+                
+                ////////////////need to fix. it does not create a new docs in collection//////////////
+                firebaseDb.db
+                .collection('friendlist')
+                .doc(user.uid)
+                .set({
+                    friendlist: []
+                }).catch(error => this.setState({errorMessage: error.message}));
+                ////////////////////////////////////////////////////////////////////////////////
+                return user.updateProfile({
+                    displayName: username
+                })
+             }).catch(error => this.setState({errorMessage: error.message}));
+    }
+        
+    render(){
+
+        const {username, email, password, signUpSuccess, errorMessage, gender, age, location, occupation, interests} = this.state
+
+        return(
+
+            <SafeAreaView style={styles.container}>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <StatusBar
+                        barstyle="light-content"
+                    />
+                    <View style={styles.uploadIcon}>
+                        <Ionicons name="ios-add" size={48} color="#00ff55" style={{paddingBottom: 180}} ></Ionicons>
+                    </View>
+
+                    <View style={styles.inputInfo}>
+                        <View style={styles.errorMessage}>
+                            {errorMessage && <Text style={styles.error}>{errorMessage}</Text>}
+                        </View>
+                        <TextInput
+                            placeholder="Username"  
+                            onChangeText={this.handleUpdateUsername}
+                            value={username}
+                            placeholderTextColor = "rgba(255,255,255,0.7)"
+                            returnKeyType="next"
+                            //onSubmitEditing={() => this.emailInput.focus()}
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            style = {styles.input}
+                        />
+
+                        <TextInput
+                            placeholder="Email"
+                            onChangeText={this.handleUpdateEmail}
+                            value={email}
+                            placeholderTextColor = "rgba(255,255,255,0.7)"
+                            returnKeyType="next"
+                            //onSubmitEditing={() => this.passwordInput.focus()}
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            style = {styles.input}
+                            //ref={(input) => this.emailInput = input}
+                        />
+
+                        <TextInput
+                            placeholder="Password"
+                            onChangeText={this.handleUpdatePassword}
+                            value={password}
+                            placeholderTextColor = "rgba(255,255,255,0.7)"
+                            returnKeyType="next"
+                            //onSubmitEditing={() => this.signUp.focus()}
+                            secureTextEntry
+                            style = {styles.input}
+                            //ref={(input) => this.passwordInput = input}
+                        />
+
+                        <TextInput
+                            placeholder="Gender"
+                            onChangeText={this.handleUpdateGender}
+                            value={gender}
+                            placeholderTextColor = "rgba(255,255,255,0.7)"
+                            returnKeyType="next"
+                            //onSubmitEditing={() => this.signUp.focus()}
+                            //secureTextEntry
+                            style = {styles.input}
+                            //ref={(input) => this.passwordInput = input}
+                        />
+
+                        <TextInput
+                            placeholder="Age"
+                            onChangeText={this.handleUpdateAge}
+                            value={age}
+                            placeholderTextColor = "rgba(255,255,255,0.7)"
+                            returnKeyType="next"
+                            keyboardType= "numeric"
+                            //onSubmitEditing={() => this.signUp.focus()}
+                            //secureTextEntry
+                            style = {styles.input}
+                            //ref={(input) => this.passwordInput = input}
+                        />
+
+                        <TextInput
+                            placeholder="Location"
+                            onChangeText={this.handleUpdateLocation}
+                            value={location}
+                            multiline
+                            placeholderTextColor = "rgba(255,255,255,0.7)"
+                            returnKeyType="next"
+                            //onSubmitEditing={() => this.signUp.focus()}
+                        // secureTextEntry
+                            style = {styles.input}
+                        // ref={(input) => this.passwordInput = input}
+                        />
+
+                        <TextInput
+                            placeholder="Occupation"
+                            onChangeText={this.handleUpdateOccupation}
+                            value={occupation}
+                            placeholderTextColor = "rgba(255,255,255,0.7)"
+                            returnKeyType="next"
+                            //onSubmitEditing={() => this.signUp.focus()}
+                            //secureTextEntry
+                            style = {styles.input}
+                            //ref={(input) => this.passwordInput = input}
+                        /> 
+
+                        <TextInput
+                            placeholder="Interests/Hobbies"
+                            onChangeText={this.handleUpdateInterests}
+                            value={interests}
+                            multiline
+                            placeholderTextColor = "rgba(255,255,255,0.7)"
+                            returnKeyType="go"
+                            //onSubmitEditing={() => this.signUp.focus()}
+                            //secureTextEntry
+                            style = {styles.input}
+                            //ref={(input) => this.passwordInput = input}
+                        />        
+
+                        <TouchableOpacity style = {styles.buttonContainer} 
+                            onPress={() => {
+                                this.handleCreateUser()
+                            }}
+                            ref={(input) => this.signUp = input}>
+                            <Text style={styles.buttonText}>Sign Up</Text>
+                        </TouchableOpacity>
+                        {
+                            signUpSuccess && <Text style={styles.buttonText}>Sign Up Successful</Text>
+                        }
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 15,
+        backgroundColor: "black",
+        justifyContent:'center',
+    },
+
+    input: {
+        height: 40,
+        backgroundColor: 'rgba(255,255,255,0.2)',
+        marginBottom: 10,
+        color: "#FFF",
+        paddingHorizontal: 10
+    },
+
+    buttonContainer: {
+        backgroundColor: '#2980b9',
+        paddingVertical: 15
+    },
+
+    buttonText: {
+        textAlign: 'center',
+        color: '#FFFFFF',
+        fontWeight: '700'
+    },
+
+    errorMessage: {
+        height: 72,
+        alignItems: "center",
+        justifyContent: "center",
+        marginHorizontal: 30
+    },
+
+    error: {
+        color: "#E9446A",
+        fontSize: 13,
+        fontWeight: "600",
+        textAlign: "center"
+    },
+
+    uploadIcon: {
+        alignItems: 'center',
+    },
+
+    inputInfo: {
+        
+    }
+})
+
+
+export default withNavigation(RegisterForm);
+
+
