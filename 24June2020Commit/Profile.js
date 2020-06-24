e5f7b423fc1688cf27660711cd90bfb4ddac5914
Asuraxsoul <54731174+Asuraxsoul@users.noreply.github.com>
2020-06-24 20:13:51 +0800

    Update Profile.js

--- 24June2020Commit/Profile.js
+++ 24June2020Commit/Profile.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {StyleSheet, View, Text, TextInput, SafeAreaView, Image, ScrollView, TouchableOpacity, AsyncStorage, StatusBar} from 'react-native';
+import { StyleSheet, View, Text, TextInput, SafeAreaView, Image,
+         ScrollView, TouchableOpacity, AsyncStorage, StatusBar, ImageBackground } from 'react-native';
 import {Ionicons, MaterialIcons} from "@expo/vector-icons";
-//import {withNavigation} from 'react-navigation';
 import firebaseDb from '../Database/firebaseDb';
 import {Thumbnail} from 'native-base';
 
 
 export default class Profile extends React.Component {
-    
     state = {
         uid: this.props.navigation.getParam("uid",""),
         username: this.props.navigation.getParam("displayName",""),
@@ -24,7 +23,7 @@ export default class Profile extends React.Component {
         const user = firebaseDb.auth.currentUser;
         const uid = user.uid;
         this.setState({uid: uid})
-        firebaseDb.db.collection('profile').doc(/*this.state.*/uid).onSnapshot(docSnapshot => {
+        firebaseDb.db.collection('profile').doc(uid).onSnapshot(docSnapshot => {
             const info = docSnapshot.data()
             this.setState({
                 username: info.username,
@@ -37,26 +36,13 @@ export default class Profile extends React.Component {
                 photo: info.photo
             })
         })
-        
-        /*firebaseDb.db.collection('profile').get().then(querySnapshot => {
-        const results = []
-        querySnapshot.docs.map(documentSnapshot => results.push(documentSnapshot.data()))
-        const profile = results.filter(profile => profile.username == this.state.username);
-        this.setState({
-            username: profile[0].username,
-            email: profile[0].email,
-            gender: profile[0].gender,
-            age: profile[0].age,
-            location: profile[0].location,
-            occupation: profile[0].occupation,
-            interests: profile[0].interests,
-        })
-        console.log(results)
-        }).catch(err => console.error(err))*/
+    }
+
+    handleSignOutUser = () => {
+        firebaseDb.auth.signOut();
     }
 
     render(){
-
         const pressHandle = () => {
             this.props.navigation.navigate('EditProfile', {info: this.state});
         }
@@ -64,48 +50,62 @@ export default class Profile extends React.Component {
         const {username, email, gender, age, location, occupation, interests, photo} = this.state;//.profile;
         return(
             <SafeAreaView style={styles.container}>
-                <ScrollView showVerticalScrollIndicator={false}>
-                    <View style={styles.title}>
-                    <Ionicons name="ios-arrow-back" size={24} color="black"></Ionicons>
-                    <Ionicons name="md-more" size={24} color="black"></Ionicons>
-                    </View>
-                    <View style={{alignSelf:"center"}}>
-    
-                        <View style={styles.profileImage}>
-                            <Thumbnail source={{uri: photo}}
-                             style={styles.image} resizeMode="center">
-                             </Thumbnail>
+                    <TouchableOpacity
+                        style={styles.backArrow}
+                        onPress={() => this.props.navigation.goBack()}
+                    >
+                        <Ionicons name="md-arrow-back" size={24} color='#73788B'></Ionicons>
+                    </TouchableOpacity>
+
+                    <View style={{marginTop: 24, alignItems: 'center'}}>
+                        <View style={styles.avatarContainer}>
+                            <Image
+                               source={photo? {uri: photo} :
+                                      {uri: 'https://f0.pngfuel.com/png/981/645/default-profile-picture-png-clip-art.png'}}
+                               style={styles.profileImage}
+                            />
                         </View>
-    
-                        <View style={styles.dm}>
-                            <TouchableOpacity>
-                                <MaterialIcons name="chat" size={18} color="grey"></MaterialIcons>
-                            </TouchableOpacity>
+
+                        <Text style={styles.userStyle}>{username}</Text>
+
+                        <View style={styles.infoContainer}>
+                            <View style={styles.info}>
+                                <Text style={styles.title}>Gender</Text>
+                                <Text style={styles.text}>{gender}</Text>
+                            </View>
+                            <View style={styles.info}>
+                                <Text style={styles.title}>Location</Text>
+                                <Text style={styles.text}>{location}</Text>
+                            </View>
+                            <View style={styles.info}>
+                                <Text style={styles.title}>Occupation</Text>
+                                <Text style={styles.text}>{occupation}</Text>
+                            </View>
+                            <View style={styles.info}>
+                                <Text style={styles.title}>Age</Text>
+                                <Text style={styles.text}>{age}</Text>
+                            </View>
                         </View>
-    
-                        <View style={styles.active}></View>           
-                    </View>
-                    <View style={styles.infoContainer}>
-                        <Text style={[styles.text, {color: "black", fontWeight: "600", fontSize: 24}]}> Username: {username} </Text>
-                        <Text style={[styles.text, {color: "black", fontSize: 14}]}> Gender: {gender} </Text>
-                        <Text style={[styles.text, {color: "black", fontSize: 14}]}> Location: {location} </Text>
-                        <Text style={[styles.text, {color: "black", fontSize: 14}]}> Occupation: {occupation} </Text>
-                        <Text style={[styles.text, {color: "black", fontSize: 14}]}> Interests/Hobbies: {interests} </Text>
-                        <Text style={[styles.text, {color: "black", fontSize: 14}]}> Age: {age} </Text>
+                        <View style={styles.info}>
+                            <Text style={styles.title}>Interests/Hobbies</Text>
+                            <Text style={styles.text}>{interests}</Text>
+                        </View>
+
+                        <TouchableOpacity
+                            style={styles.logoutS}
+                            onPress={this.handleSignOutUser}
+                        >
+                            <Text style={styles.logoutText}>Log Out</Text>
+                        </TouchableOpacity>
+
                         <View style={styles.buttonsContainer}>
                             <TouchableOpacity style = {styles.buttonContainer}
                                 onPress={pressHandle}
                             >
                                 <Text style={styles.buttonText}>Edit</Text>
                             </TouchableOpacity>
-                            <TouchableOpacity style = {styles.buttonContainer}
-                                onPress={() => this.props.navigation.navigate("HomeScreen")}
-                            >
-                                <Text style={styles.buttonText}>Change</Text>
-                            </TouchableOpacity>
                         </View>
                     </View>
-                </ScrollView> 
             </SafeAreaView>
         )
     }
@@ -114,104 +114,68 @@ export default class Profile extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#b3ffda",
-        alignItems: "center",
-        justifyContent: "center"
     },
-
-    input: {
-        height: 40,
-        backgroundColor: 'rgba(255,255,255,0.2)',
-        marginBottom: 10,
-        color: "#FFF",
-        paddingHorizontal: 10
+    backArrow: {
+        padding: 10
     },
-
-    image: {
-        flex: 1,
-        width: undefined,
-        height: undefined
+    avatarContainer: {
+        shadowColor: '#151734',
+        shadowRadius: 30,
+        shadowOpacity: 0.4,
+        borderRadius: 68
     },
-
-    title: {
-        marginTop: 24,
-        marginHorizontal: 16,
-        justifyContent: "space-between",
-        flexDirection: "row"
-    },
-
-    text: {
-        textAlign: 'center',
-        color: 'black',
-        fontWeight: '700',
-        //fontFamily: "HelveticaNeue"
-    },
-
     profileImage: {
-        width: 200,
-        height: 200,
-        borderRadius: 100,
-        overflow:"hidden"
+        width: 136,
+        height: 136,
+        borderRadius: 68
     },
-
-    dm: {
-        backgroundColor: "white",
-        position: "absolute",
-        top: 20,
-        width: 40,
-        height: 40,
-        borderRadius: 20,
-        alignItems: "center",
-        justifyContent: "center"
+    userStyle: {
+        marginTop : 24,
+        fontSize: 16,
+        fontWeight: '600'
     },
-
-    active: {
-        backgroundColor: "#00ff55",
-        position: "absolute",
-        bottom: 28,
-        left: 10,
-        padding: 4,
-        height: 20,
-        width: 20,
-        borderRadius: 10
-    },
-
-    add: {
-        backgroundColor: "white",
-        position: "absolute",
-        bottom: 0,
-        right: 0,
-        width: 60,
-        height: 60,
-        borderRadius: 30,
-        alignItems: "center",
-        justifyContent:"center"
-    },
-
     infoContainer: {
-        alignSelf: "center",
-        alignItems: "center",
-        marginTop: 16
+        flexDirection: 'row',
+        width: 300,
+        justifyContent: 'space-between',
+        margin: 32
     },
-
+    info: {
+        alignItems: 'center',
+        flex: 1
+    },
+    title: {
+        color: '#C3C5CD',
+        fontSize: 14,
+        fontWeight: '500'
+    },
+    text: {
+        marginTop: 4,
+        color: '#4F566D',
+        fontSize: 18,
+        fontWeight: '300'
+    },
+    logoutS: {
+        marginTop: 50,
+    },
+    logoutText: {
+        fontSize: 14,
+        fontWeight: '500',
+        color: '#129cff'
+    },
     buttonContainer: {
-        //paddingTop: 20,
-        backgroundColor: '#2980b9',
+        backgroundColor: '#73788B',
         paddingVertical: 15,
         marginTop: 10,
+        borderRadius: 10
     },
-
     buttonsContainer: {
-        paddingTop: 100,
-      
+        paddingTop: 15,
     },
-
     buttonText: {
         textAlign: 'center',
-        color: '#FFFFFF',
-        fontWeight: '700',
+        color: 'white',
+        fontWeight: '500',
         width: 200,
     }
 })
-
-//export default withNavigation(Profile);
