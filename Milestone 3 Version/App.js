82a27ddd60bebb3600db758723a7c6343c394a9f
Jaylenlee <54731199+Jaylenlee@users.noreply.github.com>
2020-07-09 11:19:06 +0800

    Add files via upload

---
+++ Milestone 3 Version/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './src/Components/Route/Navigation'; 
+
+export default function App() {
+  console.disableYellowBox = true;
+  return (
+    <Navigation/>
+  )
+}
+
