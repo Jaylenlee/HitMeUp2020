c32497fed734ec7ef4c493b0e5edd9e668c368b2
Jaylenlee <54731199+Jaylenlee@users.noreply.github.com>
2020-07-26 15:22:30 +0800

    remove 1st alert

--- Milestone 3 Version/src/Components/Event/CreateEventContainer.js
+++ Milestone 3 Version/src/Components/Event/CreateEventContainer.js
@@ -193,11 +193,6 @@ class CreateEventContainer extends React.Component {
                                         estimatedSize.length && activityDetails.length && (isPrivate || isPublic)) {
                                         this.handleSelectInvitees()
                                     }
-                 
-
-                                    if (isPublic) {
-                                        alert("Your PUBLIC event has been created and will be reflected under Public Feeds")
-                                    }
                                 }}
                             >
                                 <Text style={styles.nextButtonText}>Next</Text>
