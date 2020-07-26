59cbeb8a663870e9c5c7a6c43f5957ffef1511ab
Asuraxsoul <54731174+Asuraxsoul@users.noreply.github.com>
2020-07-26 12:30:03 +0800

    Add files via upload
    Changed Attendance display

--- Milestone 3 Version/src/Components/Notification/ViewInviteAttendance.js
+++ Milestone 3 Version/src/Components/Notification/ViewInviteAttendance.js
@@ -146,44 +146,44 @@ export default class ViewInviteAttendance extends React.Component{
                         data={data}
                         width={Dimensions.get("window").width}
                         height={220}
-                        chartConfig= {chartConfig}
+                        chartConfig={chartConfig}
                         accessor="population"
                         backgroundColor="transparent"
                         paddingLeft="15"
                         absolute
                     />
-                   </View> 
+                   </View>
                 </View>
                 <View style={styles.list}>
-                    <View>
-                        <Text> Attending </Text>
+                    <View style={{alignItems: 'center'}}>
+                        <Text style={styles.title}>Attending</Text>
                         <FlatList
-                        style={styles.feed}
-                        data={attendees}
-                        renderItem={({ item }) => this.renderEvent(item)}
-                        keyExtractor={item => item.eventName}
-                        showsVerticalScrollIndicator={false}
-                        /> 
+                            style={styles.feed}
+                            data={attendees}
+                            renderItem={({ item }) => this.renderEvent(item)}
+                            keyExtractor={item => item.eventName}
+                            showsVerticalScrollIndicator={false}
+                        />
                     </View>
-                    <View>
-                        <Text> Not Attending </Text>
+                    <View style={{marginHorizontal: 30, alignItems: 'center'}}>
+                        <Text style={styles.title}>Not Attending</Text>
                         <FlatList
-                        style={styles.feed}
-                        data={nonAttendees}
-                        renderItem={({ item }) => this.renderEvent(item)}
-                        keyExtractor={item => item.eventName}
-                        showsVerticalScrollIndicator={false}
-                        /> 
+                            style={styles.feed}
+                            data={nonAttendees}
+                            renderItem={({ item }) => this.renderEvent(item)}
+                            keyExtractor={item => item.eventName}
+                            showsVerticalScrollIndicator={false}
+                        />
                     </View>
-                    <View>
-                        <Text> Pending </Text>
+                    <View style={{alignItems: 'center'}}>
+                        <Text style={styles.title}>Pending</Text>
                         <FlatList
-                        style={styles.feed}
-                        data={invitees}
-                        renderItem={({ item }) => this.renderEvent(item)}
-                        keyExtractor={item => item.eventName}
-                        showsVerticalScrollIndicator={false}
-                        /> 
+                            style={styles.feed}
+                            data={invitees}
+                            renderItem={({ item }) => this.renderEvent(item)}
+                            keyExtractor={item => item.eventName}
+                            showsVerticalScrollIndicator={false}
+                        />
                     </View>
                 </View>
             </ScrollView>
@@ -210,5 +210,21 @@ const styles = StyleSheet.create({
     },
     list: {
         flexDirection: "row",
+        alignSelf: 'center',
+        marginTop: 20
     },
+    title: {
+        fontSize: 16,
+        fontWeight: '300',
+        color: '#455A64',
+        marginBottom: 5
+    },
+    nameBox: {
+        marginBottom: 4,
+        alignItems: 'center'
+    },
+    naming: {
+        fontSize: 16,
+        fontWeight: '400'
+    }
 })
